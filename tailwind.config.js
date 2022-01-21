fb3942ef17c1ba41a41aecfce1ed29c48e3a6578
Luigi <luigicruz.dev@gmail.com>
2022-01-21 17:49:55 +0800

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
+  content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
